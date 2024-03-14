0949724f0b410466587fbfb13a7c8af5687dd676
Mário Cabral Nkandu <160603454+Marionkandu@users.noreply.github.com>
2024-03-14 14:24:28 +0000

    criando o arquivo operadores aritmetios

---
+++ op-aritmeticos.js
@@ -0,0 +1,22 @@
+let numero1 = 20;
+let numero2 =30;
+
+let soma = numero1 + numero2;
+
+let subtração = numero1 - numero2;
+
+let multiplicacao = numero1 * numero2;
+
+let divisao = numero1 / numero2;
+
+let resto = numero1 % numero2;
+
+console.log(`Os numeros das operacões são ${numero1} e ${numero2} `);
+console.log(`A soma dos numeros é ${numero1 + numero2} `);
+console.log(`A subtração dos numeros é ${numero1 - numero2}`);
+console.log(`A multiplicacao dos ${numero1 * numero2} `);
+console.log(`A divisao dos numeros é ${numero1 / numero2} `);
+console.log(`O resto dos numeros é ${numero1 % numero2} `);
+
+
+
